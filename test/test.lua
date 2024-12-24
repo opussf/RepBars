6a51aa8aba8f88094537c55f832deacd16924610
opussf <github@zz9-za.com>
2024-12-23 18:18:56 -0800

    Test and stubs update

--- test/test.lua
+++ test/test.lua
@@ -32,52 +32,52 @@ function test.test_FactionBar_sessionStartIsSet()
 	assertEquals( time(), FB.sessionStart )
 end
 function test.test_FactionGainEvent_Increase_Normal_Faction_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire increased by 92" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire increased by 92." )
 	assertTrue( FB_repSaved["Darkmoon Faire"] )
 end
 function test.test_FactionGainEvent_Increase_Normal_Value_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire increased by 50" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire increased by 50." )
 	now = time()
 	testValue = FB_repSaved["Darkmoon Faire"][now] or FB_repSaved["Darkmoon Faire"][now-1]
 	assertEquals( 50, testValue )
 end
 function test.test_FactionGainEvent_Decrease_Normal_Faction_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire decreased by 25" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire decreased by 25." )
 	assertTrue( FB_repSaved["Darkmoon Faire"] )
 end
 function test.test_FactionGainEvent_Decrease_Normal_Value_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire decreased by 69" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Darkmoon Faire decreased by 69." )
 	now = time()
 	testValue = FB_repSaved["Darkmoon Faire"][now] or FB_repSaved["Darkmoon Faire"][now-1]
 	assertEquals( -69, testValue )
 end
 function test.test_FactionGainEvent_Increase_AccountWide_Faction_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name increased by 16" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name increased by 16." )
 	assertTrue( FB_repSaved["Find my Name"] )
 end
 function test.test_FactionGainEvent_Increase_AccountWide_Value_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name increased by 5" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name increased by 5." )
 	now = time()
 	testValue = FB_repSaved["Find my Name"][now] or FB_repSaved["Find my Name"][now-1]
 	assertEquals( 5, testValue )
 end
 function test.test_FactionGainEvent_Decrease_AccountWide_Faction_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name decreased by 11" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name decreased by 11." )
 	assertTrue( FB_repSaved["Find my Name"] )
 end
 function test.test_FactionGainEvent_Decrease_AccountWide_Value_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name decreased by 7" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Your Warband's reputation with Find my Name decreased by 7." )
 	now = time()
 	testValue = FB_repSaved["Find my Name"][now] or FB_repSaved["Find my Name"][now-1]
 	assertEquals( -7, testValue )
 end
 function test.notest_FactionGainEvent_Increase_Guild_Faction_Captured()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Guild decreased by 14" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Guild decreased by 14." )
 	assertTrue( FB_repSaved["Test Guild"] )
 end
 function test.test_FactionGainEvent_Increase_Normal_Value_Captured_multipleValues()
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Stormwind increased by 101" )
-	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Stormwind increased by 202" )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Stormwind increased by 101." )
+	FB.FactionGainEvent( FB_Frame, "repEvent", "Reputation with Stormwind increased by 202." )
 	now = time()
 	testValue = FB_repSaved["Stormwind"][now] or FB_repSaved["Stormwind"][now-1]
 	assertEquals( 303, testValue )
